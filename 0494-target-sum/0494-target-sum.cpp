e75566da51a22d1d25582abe6e74ec1a11f2b993
Ashah_15.0 <aaravshah091@gmail.com>
2026-09-08 19:48:30 +0530

    Time: 18 ms (44.95%), Space: 43.1 MB (13.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-
+    vector<vector<int>>dp;
     int func(int i,int curr,vector<int>&nums,int target){
         if(i>=nums.size()){
-            if(curr==target){return 1;}
+            if(curr==(target)){return 1;}
             else return 0;
         }
+        if(dp[i][1000+curr]!=-1){return dp[i][1000+curr];}
         int ways=0;
         ways=func(i+1,curr-nums[i],nums,target)+func(i+1,curr+nums[i],nums,target);
-        return ways;
+        return dp[i][1000+curr]=ways;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        dp.assign(n+1,vector<int>(2005,-1));
         int ans=func(0,0,nums,target);
         return ans;
     }
